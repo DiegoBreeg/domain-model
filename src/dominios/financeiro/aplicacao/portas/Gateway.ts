0174fe4d3d17f577535ff82265f32e4fafd01fd0
diegobreeg <diegobreeg@gmail.com>
2026-01-14 19:14:55 -0300

    adicionando infra

--- src/dominios/financeiro/aplicacao/portas/Gateway.ts
+++ src/dominios/financeiro/aplicacao/portas/Gateway.ts
@@ -1,31 +1,4 @@
-export enum FormaDePagamento {
-    CARTAO = "CARTAO",
-    PIX = "PIX",
-    BOLETO = "BOLETO",
-}
-
-export enum Ciclo {
-    SEMANAL = "SEMANAL",
-    MENSAL = "MENSAL",
-    ANUAL = "ANUAL",
-}
-
-export type CriarAssinaturaGateway = {
-    assinaturaId: string;
-    clienteId: string;
-    valor: number;
-    vencimento: Date;
-    formaDePagamento: FormaDePagamento;
-    ciclo: Ciclo;
-}
-
-export type CriarClienteGateway = {
-    clienteId: string;
-}
-
-export type identificadorExterno = string;
-
 export interface Gateway {
-    criarAssinatura(dados: CriarAssinaturaGateway): Promise<identificadorExterno>;
-    criarCliente(dados: CriarClienteGateway): Promise<identificadorExterno>;
+    criarCliente(clienteId: string): Promise<string>;
+    criarAssinatura(assinaturaId: string): Promise<string>;
 }
